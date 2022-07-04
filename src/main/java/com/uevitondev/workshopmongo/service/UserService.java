fa61d5eb714a51b88a19e32216c7efa3e2a27361
uevitondev <uevitonsantos@outlook.com>
2022-07-04 20:27:59 -0300

    Inserção de usuário com POST

--- src/main/java/com/uevitondev/workshopmongo/service/UserService.java
+++ src/main/java/com/uevitondev/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uevitondev.workshopmongo.domain.User;
+import com.uevitondev.workshopmongo.dto.UserDTO;
 import com.uevitondev.workshopmongo.repository.UserRepository;
 import com.uevitondev.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -23,6 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insertUser(User obj) {
+		return userRepository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+
 	}
 
 }

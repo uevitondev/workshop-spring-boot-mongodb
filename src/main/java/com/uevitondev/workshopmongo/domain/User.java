79c89a167b0a48b3e9bb470a6f5863f455076947
uevitondev <uevitonsantos@outlook.com>
2022-07-04 17:58:34 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/uevitondev/workshopmongo/domain/User.java
+++ src/main/java/com/uevitondev/workshopmongo/domain/User.java
@@ -8,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
 public class User implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id

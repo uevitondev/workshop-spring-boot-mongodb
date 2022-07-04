d019fd3fe507c89c5d2fb5eba28dab7f929fe2f2
uevitondev <uevitonsantos@outlook.com>
2022-07-04 19:46:00 -0300

    Obtendo um usuário por id

--- src/main/java/com/uevitondev/workshopmongo/service/UserService.java
+++ src/main/java/com/uevitondev/workshopmongo/service/UserService.java
@@ -1,21 +1,28 @@
 package com.uevitondev.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uevitondev.workshopmongo.domain.User;
 import com.uevitondev.workshopmongo.repository.UserRepository;
+import com.uevitondev.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }

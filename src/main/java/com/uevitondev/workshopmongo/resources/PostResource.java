2a840275d730823000a390cb0b4470e2158a1554
uevitondev <uevitonsantos@outlook.com>
2022-07-05 18:02:42 -0300

    Consulta simples com query methods

--- src/main/java/com/uevitondev/workshopmongo/resources/PostResource.java
+++ src/main/java/com/uevitondev/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.uevitondev.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uevitondev.workshopmongo.domain.Post;
+import com.uevitondev.workshopmongo.resources.util.URL;
 import com.uevitondev.workshopmongo.service.PostService;
 
 @RestController
@@ -22,6 +26,15 @@ public class PostResource {
 
 		Post obj = postService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+
+		text = URL.decodeParam(text);
+		List<Post> listPost = postService.findByTitle(text);
+
+		return ResponseEntity.ok().body(listPost);
 	}
 
 }

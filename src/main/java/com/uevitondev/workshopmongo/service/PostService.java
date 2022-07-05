80b273f6c006ba857b47c0f738ca5589480e740d
uevitondev <uevitonsantos@outlook.com>
2022-07-05 17:00:15 -0300

    Obtendo um post por id

---
+++ src/main/java/com/uevitondev/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.uevitondev.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uevitondev.workshopmongo.domain.Post;
+import com.uevitondev.workshopmongo.repository.PostRepository;
+import com.uevitondev.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
